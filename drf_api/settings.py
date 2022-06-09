32288ec5d6051e9fbd6368d7e868cdaa4a334340
Tim Schulz <tim@getbej.com>
2022-06-09 12:56:05 +0000

    Changed DEBUG status for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -35,8 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
